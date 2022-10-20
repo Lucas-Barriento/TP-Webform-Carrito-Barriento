a94da85c2f3256e704d26f79368d95a36d1f8160
Lucas Barriento <lucas.barriento@gmail.com>
2022-10-20 00:08:18 -0300

    Se agrego la lista de objetos que corresponden al carrito con la cantidad, si se repite la cantidad aumenta 1 unidad

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -15,22 +15,57 @@ namespace TPWebformCarrito_BARRIENTO
     public partial class Default : System.Web.UI.Page
     {
         public List<Articulo> ListaArticulo { get; set; }
-        public string algo { get; set; }
+        public List<ArticuloCarrito> ListaCarrito = new List<ArticuloCarrito>();
+        public int idSeleccionado { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
+            ListaCarrito = CarritoSession();
+            bool existente = false;
             ArticuloNegocio negocio= new ArticuloNegocio();
             ListaArticulo = negocio.Listar();
-            //dgvArticulos.DataSource = negocio.Listar();
-            //dgvArticulos.DataBind();
-            algo = Request.QueryString["id"].ToString();
-            
+
+            if (Request.QueryString["id"] != null)
+            {
+                idSeleccionado = int.Parse(Request.QueryString["id"]);
+                ArticuloCarrito aux = new ArticuloCarrito();
+
+                foreach ( var art in ListaCarrito)
+                {
+                     if (art.Id == idSeleccionado)
+                    {
+                        art.Cantidad++;
+                        existente = true;
+                        //envia cambios a session
+                        Session.Add("ArticulosEnCarrito", ListaCarrito);
+                    }
+                }
+                if (!existente)
+                {   // con el idSeleccionado busca el objeto a agregar  
+                    Articulo obj = ListaArticulo.Find((x) => x.Id == idSeleccionado);
+                    //aux.Id=idSeleccionado;
+                    aux.Id = idSeleccionado;
+                    aux.Descripcion = (string)obj.Descripcion;
+                    aux.Nombre = (string)obj.Nombre;
+                    aux.Precio = (decimal)obj.Precio;
+                    aux.ImagenUrl = (string)obj.ImagenUrl;
+                    aux.Codigo = (string)obj.Codigo;
+                    aux.categoria = (Categoria)obj.categoria;
+                    aux.marca = (Marca)obj.marca;
+                    aux.Cantidad = (int)1;
+                    ListaCarrito.Add(aux);
+                    Session.Add("ArticulosEnCarrito", ListaCarrito);
+
+                }
+            }   
+
         }
 
-        //protected void dgvArticulos_SelectedIndexChanged(object sender, EventArgs e)
-        //{
-        //    var id =  dgvArticulos.SelectedRow.Cells[0];
-        //    var id2 = dgvArticulos.SelectedDataKey.Value.ToString();
-        //}
+        //se fija si en session ya hay una lista de art y si no, le asigna una vacia
+        public List<ArticuloCarrito> CarritoSession()
+        {
+            List<ArticuloCarrito> Carrito = Session["ArticulosEnCarrito"] != null ? (List<ArticuloCarrito>)(Session["ArticulosEnCarrito"]) : new List<ArticuloCarrito>();
+            return Carrito;
+        }
 
     }
 }
