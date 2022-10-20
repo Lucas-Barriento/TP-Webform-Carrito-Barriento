e4841d2800662f7269727689e3383778b09152a6
Lucas Barriento <lucas.barriento@gmail.com>
2022-10-20 01:10:38 -0300

    Se agregó el boton "quitar" que saca el elemento en su totalidad del carrito.

--- Site.Master.cs
+++ Site.Master.cs
@@ -4,15 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using Dominio;
+using Negocio;
 
 namespace TPWebformCarrito_BARRIENTO
 {
     public partial class SiteMaster : MasterPage
     {
+        public int idSeleccionado { get; set; }
+        public List<ArticuloCarrito> ListaCarrito = new List<ArticuloCarrito>();
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Request.QueryString["idQuitar"] != null)
+            {
+                idSeleccionado = int.Parse(Request.QueryString["idQuitar"]);
+                ListaCarrito = CarritoSession();
+                Session.Add("ArticulosEnCarrito",(ListaCarrito.FindAll(x => x.Id != idSeleccionado)));
+            }
         }
+        public List<ArticuloCarrito> CarritoSession()
+        {   
+            List<ArticuloCarrito> Carrito = Session["ArticulosEnCarrito"] != null ? (List<ArticuloCarrito>)(Session["ArticulosEnCarrito"]) : new List<ArticuloCarrito>();
+            return Carrito;
+        }
+
     }
 }
